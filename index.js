799be3f185af611c47d9c9770e41d68173803fc9
talhaunas1 <mtalha4600@gmail.com>
2022-08-18 00:36:21 +0500

    complete signup auth

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ app.use(express.json());
 // app.use("/api/comment", commentRoutes);
 app.use("/api/auth", authRoutes);
 
+//error throw
+app.use((err,req,res,next)=>{
+  const status = err.status || 500;
+  const message = err.message || "something went wrong!";
+  return res.status(status).json({
+    success : false,
+    status,
+    message
+  })
+})
 
 app.listen(8800, () => {
   connect();
@@ -34,15 +44,6 @@ app.listen(8800, () => {
 });
 
 
-//error throw
-app.use((err,req,res,next)=>{
-  const status = err.status || 500;
-  const message = err.message || "something went wrong!";
-  return res.status(status).jason({
-    success : false,
-    status,
-    message
-  })
-})
+
 
 
